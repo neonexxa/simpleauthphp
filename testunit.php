3eed8cd2be6875511e91c9024b9bc7cc9a9253af
neonexxa <firdaus@cyberhax.club>
2017-02-17 16:17:04 +0800

    test

--- testunit.php
+++ testunit.php
@@ -4,7 +4,7 @@ class Test extends PHPUnit_Framework_TestCase
 	public function testOnePlusOne() {
 		
 
-		$this->assertEquals(,2);
+		$this->assertEquals(2,2);
   	}
 
   	public function testindex()
